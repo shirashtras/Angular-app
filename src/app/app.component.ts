89fd5e1bbd221efaa744227a7c0d3932a952cdc4
שירה שטרסברג <ShiraStras@smg.local>
2026-02-11 17:15:36 +0200

    Les1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Les1Component } from './les1/les1.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Les1Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Angular-app';
+  p="המשימות שלי:"
+
 }
---
+++ src/app/les1/les1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-les1',
+  imports: [],
+  templateUrl: './les1.component.html',
+  styleUrl: './les1.component.css'
+})
+export class Les1Component {
+}
