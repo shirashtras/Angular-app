d3983203263c81467891e39781a2eafd23ab5aad
שירה שטרסברג <ShiraStras@smg.local>
2026-02-25 16:09:36 +0200

    Les2-in class

--- src/app/les1/les1.component.ts
+++ src/app/les1/les1.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-les1',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './les1.component.html',
   styleUrl: './les1.component.css'
 })
 export class Les1Component {
+  name:string="שירה"
+  task:string="שיעורי בית"
+  keep:string=""
+  flag:boolean=false;
+
+  save(){
+    this.task=this.keep;
+    this.keep=""
+  if(this.task=="")
+    this.flag=!this.flag
+  }
 }
